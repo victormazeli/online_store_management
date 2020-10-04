a6e554cc1276cf02a9b727763021a4990d7b76e4
Mazeli Victor <victor.vic.mazeli@gmail.com>
2020-10-04 03:53:34 +0100

    changed settings

--- api/settings.py
+++ api/settings.py
@@ -83,7 +83,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    
 }
 
 AUTH_USER_MODEL = 'users.CustomUser'
